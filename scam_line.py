92a25fd68cc7d236707218c30e0a80280a588e0d
Ckang013 <handbosomey2@gmail.com>
2025-02-24 16:40:28 +0800

    Initial commit

---
+++ scam_line.py
@@ -0,0 +1,56 @@
+"""
+    2025.02.24 反詐騙資料- 詐騙line id
+    原本在 google colab執行，但會出現timeout error，詢問 chatgpt 告知有可能此端點有阻隔雲端平台的存取
+"""
+
+import requests
+import json
+
+def get_LineID():
+    url = "https://od.moi.gov.tw/api/v1/rest/datastore/A01010000C-001277-053"
+    payload = {}
+    headers = {
+    'Cookie': 'JSESSIONID=6A30578A174A6F7BBDA336C7DD6064B4'
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = response.text
+    data = json.loads(data) # 將 str 類型轉換成 dict 類型
+    records = data['result']['records']
+    lines = []
+    for record in records[:10]:
+        line = record['帳號']
+        lines.append(line)
+    return lines
+
+def main():
+    url = "https://od.moi.gov.tw/api/v1/rest/datastore/A01010000C-001277-053"
+
+    payload = {}
+    headers = {
+    'Cookie': 'JSESSIONID=6A30578A174A6F7BBDA336C7DD6064B4'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    if response.status_code == 200:
+        data = response.text
+
+        data = json.loads(data) # 將 str 類型轉換成 dict 類型
+        
+        # 取得資料的 'records' 鍵裡面的資料
+        records = data['result']['records']
+        print(f"資料筆數: {len(records)}")
+
+        # 取出line id的部分
+        lines = []
+        for record in records[:100]:
+            line = record['帳號']
+            lines.append(line)
+        print(lines)
+            
+
+    else:   
+        print(f"API 請求失敗，狀態碼: {response.status_code}")
+
+if __name__ == "__main__":
+    main()
---
+++ tkinter_line.py
@@ -0,0 +1,48 @@
+"""
+    2025.02.24 圖形化GUI介面操作
+"""
+import tkinter
+import scam_line
+
+## 視窗設定
+window = tkinter.Tk()   # 建立一個視窗物件
+window.title("GUI")     # 設定視窗標題
+window.geometry('380x400')      # 設定長寬
+window.resizable(False, False)  # 設定x, y 方向都不能縮放
+window.configure(background='#eee')   # 設定背景色灰色
+
+## 文字
+txt = '---' # 建立一個字串變數 給查詢id存放
+a = tkinter.StringVar()
+a.set(txt)
+def show():
+    a.set(entry_txt.get())
+    lines = scam_line.get_LineID()  #qaq124b
+    for line in lines:
+        if a.get() == line:
+            b.set("此帳號為詐騙id")
+            return
+    b.set("此帳號為一般id")
+
+query_txt = '---'
+b = tkinter.StringVar()
+b.set(query_txt)
+mylabel = tkinter.Label(window, text='詐騙Line ID 查詢', font=('Arial',20,'bold')).pack()  # 建立 label 標籤，加入文字，並用pack加入視窗中
+mytxt = tkinter.Label(window, textvariable=a, font=('Arial',15)).pack()
+tkinter.Label(window, textvariable=b, font=('Arial',15)).pack()
+
+## 輸入框
+entry_txt = tkinter.StringVar()
+def clear():
+    entry_txt.set('')   # 設定變數為空字串
+    entry.delete(0, 'end')  # 清空欄位內容
+
+entry = tkinter.Entry(window, textvariable=entry_txt).pack()   # 單行輸入框
+
+## 按鈕
+btn = tkinter.Button(window, text='查詢', font=('Arial', 15), padx=3, pady=3, activeforeground='#f00', command=show)   # 建立一個 Button
+btn.pack()  # 加入視窗中
+clr_btn = tkinter.Button(window, text='清除', font=('Arial', 15), padx=3, pady=3, activeforeground='#f00', command=clear)
+clr_btn.pack()
+
+window.mainloop()
